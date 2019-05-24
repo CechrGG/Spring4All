be5bd4c626aea5abb14b5f4b4fc6c7af7ea35f99
Leslie_GuoGuo <Leslie_GuoGuo@163.com>
2019-05-24 13:44:52 +0800

    test dt

--- JavaAdvanced/src/review/DataStructure.java
+++ JavaAdvanced/src/review/DataStructure.java
@@ -6,7 +6,12 @@
  */
 package review;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
 import java.util.Vector;
 
 /**
@@ -30,13 +35,7 @@ public class DataStructure {
 		}
 	};
 	
-
-	/**
-	 * @author: Leslie
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void vecTest() {
 		Vector<String> vector = new Vector<String>();
 		for(int i = 0; i < 10; i++) {
 			vector.add("0^0 " + i + ",");
@@ -45,10 +44,64 @@ public class DataStructure {
 		while(colors.hasMoreElements()) {
 			System.out.print(colors.nextElement());
 		}
+	}
+	
+	public static void stackTest() {
+		Stack<String> stack = new Stack<String>();
+		
+		stack.push("Holdooor");
+		stack.push("Brannnn");
+		stack.push("youKnowNothing");
+		stack.add("Johnnnn");
+		System.out.println(stack.peek());
+		for(String str : stack) {
+			System.out.println(str);
+		}
+		
+		int size = stack.size();
+		//这里直接用stack.size()进行循环则不对
+		for(int i = 0; i < size; i++) {
+			System.out.print(stack.pop() + "$");
+		}
+	}
+	
+	public static void listTest() {
+		//ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		LinkedList<Integer> list = new LinkedList<Integer>();
+
+		long sum = 0;
+		long timeStart = System.currentTimeMillis();
+//		for(int i = 0; i < list.size(); i++) {
+//			sum += list.get(i);
+//		}
+//		for(Integer i : list) {
+//			sum += i;
+//		}
+		Iterator<Integer> iter = list.iterator();
+		while(iter.hasNext()) {
+			sum += iter.next();
+		}
+		System.out.println(sum);
+		long timeEnd = System.currentTimeMillis();
+		System.out.println("take " + (timeEnd-timeStart) + "ms");
+	}
+	
+
+	/**
+	 * @author: Leslie
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		vecTest();
 		
 		for(int i = 0; i < Color.values().length; i++) {
 			System.out.println(Color.values()[i].getColor());
 		}
+		
+//		stackTest();
+		listTest();
 	}
 
 }
